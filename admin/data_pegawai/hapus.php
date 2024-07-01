e7286aa5164eb116ad6c865d0dbdbaddc521bb5a
ikhwan057 <ikhwanlucenzo@gmail.com>
2024-07-01 23:18:46 +0700

    part 7

---
+++ admin/data_pegawai/hapus.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require_once('../../config.php');
+
+$id = $_GET['id'];
+
+$result = mysqli_query($connection, "DELETE FROM pegawai WHERE id=$id");
+
+$_SESSION['berhasil'] = 'Data berhasil dihapus';
+header("Location: pegawai.php");
+exit;
+
+include('../layout/footer.php');
